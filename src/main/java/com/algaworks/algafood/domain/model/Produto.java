1ec7f0fd6b015aec26c6f5ed70596e4a76e1c0c9
rafael moura <rafaelfelipe.peixoto@gmail.com>
2024-03-02 23:50:11 -0300

    código atualizado aula capitulo 6 episódio 7 - Desafio: Criando um mapeamento ManyToOne

---
+++ src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private BigDecimal preco;
+
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id", referencedColumnName = "id")
+    private Restaurante restaurante;
+}
